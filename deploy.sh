df90a520963ef5762e0cf7518001a7685bab73ef
Anshu Rao <chandranshu.rao@gmail.com>
2019-06-21 22:38:53 -0500

    Fixing deploy script

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+echo Building your Vue app...
+npm run build
+
+echo "Build successful. Deploying to master"
+
+cd dist
+git init
+git add -A
+git commit -m "auto deployment"
+
+# deploy
+git push -f git@github.com:chandranshurao14/chandranshurao14.github.io.git master
+
+cd ../
+
+echo "Switching back to source branch"
+git checkout source
+
+echo "Done"
